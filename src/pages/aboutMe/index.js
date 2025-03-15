631c52a347950924d210a28be3f4ea6fb006ef8c
Caio Paiva <caiowick@hotmail.com>
2025-03-15 20:15:02 -0300

    adding about me content

--- src/pages/aboutMe/index.js
+++ src/pages/aboutMe/index.js
@@ -1,5 +1,23 @@
+import styles from "./AboutMe.module.css";
+import PostModel from "components/PostModel";
+import fotoCapa from "assets/sobre_mim_capa.png";
+import fotoSobreMim from "assets/sobre_mim_foto.jpg";
+
 export default function AboutMe() {
     return (
-        <h1>About Me</h1>
+       <PostModel
+            fotoCapa={fotoCapa}
+            titulo="Sobre mim"
+        >
+            <h3 className={styles.subtitulo}>
+                Olá, eu sou o Caio!
+            </h3>
+
+            <img 
+                src={fotoSobreMim}
+                alt="Foto de perfil"
+                className={styles.fotoSobreMim}
+            />
+       </PostModel>
     )
 }
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import Initial from "./pages/Initial";
 import AboutMe from "./pages/AboutMe";
 import Menu from "./components/Menu";
 import Footer from "components/Footer";
+import DefaultPage from "components/DefaultPage";
 
 
 function AppRoutes() {
@@ -11,8 +12,10 @@ function AppRoutes() {
       <Menu />
 
       <Routes>
-        <Route path="/" element={<Initial/>} />
-        <Route path="/aboutme" element={<AboutMe/>} />
+        <Route path="/" element={<DefaultPage />}>
+          <Route index element={<Initial/>} />
+          <Route path="aboutme" element={<AboutMe/>} />
+        </Route>
         <Route path="*" element={<div>Page not found</div>}/>
       </Routes>
       <Footer />
